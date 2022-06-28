0c5333b82f16cdd821099a96aa04cd6e438dc91f
Alexander Alexeev <alekseev@trueconf.ru>
2022-06-28 19:56:30 +0300

    adding todo item tests

--- src/todo-items/todo-items.service.spec.ts
+++ src/todo-items/todo-items.service.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoItemsService } from './todo-items.service';
+import { TodoItemEntity } from './entities/todo-item.entity';
+import { CreateTodoItemDto } from './dto/create-todo-item.dto';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const createDto: CreateTodoItemDto = {
+  title: 'some title',
+  description: 'some description',
+  isFavorite: false,
+  isComplete: false,
+};
+
+const createTodoItem = (idx: number): TodoItemEntity => {
+  return {
+    id: `item id ${idx}`,
+    title: `some title ${idx}`,
+    description: `some description ${idx}`,
+    isFavorite: false,
+    isComplete: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    _version: 1,
+    author: {
+      id: `user id ${idx}`,
+      name: `User ${idx}`,
+      email: '',
+      password: '',
+      avatar: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      _version: 1,
+    },
+  };
+};
+
+const todoItem = createTodoItem(1);
+
+const todoItemList: TodoItemEntity[] = [createTodoItem(0), createTodoItem(1), createTodoItem(2), createTodoItem(3)];
 
 describe('TodoItemsService', () => {
   let service: TodoItemsService;
+  let entity: Repository<TodoItemEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoItemsService],
+      providers: [
+        TodoItemsService,
+        {
+          provide: getRepositoryToken(TodoItemEntity),
+          useValue: {
+            create: jest.fn().mockResolvedValue(createDto),
+            findAll: jest.fn().mockResolvedValue(todoItemList),
+            findOne: jest.fn().mockResolvedValue(todoItem),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoItemsService>(TodoItemsService);
+    service = await module.get<TodoItemsService>(TodoItemsService);
+    entity = await module.get<Repository<TodoItemEntity>>(getRepositoryToken(TodoItemEntity));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // describe('create()', () => {
+  //   it('should save a todo item in the database', async () => {
+  //     const result = await service.create(createDto, 'token');
+  //     expect(result.toEqual(createDto));
+  //   });
+  // });
+
+  describe('findAll()', () => {
+    it('should return an array of todo items', async () => {
+      const list = await service.findAll('token');
+      expect(list).toEqual(todoItemList);
+    });
+  });
+
+  // describe('findOne()', () => {
+  //   it('should return a single todo item', async () => {
+  //     const repoSpy = jest.spyOn(entity, 'find');
+  //     expect(service.findOne('item id 1', 'token').resolves.toEqual(todoItem));
+  //     expect(repoSpy).toBeCalledWith({ id: 'item id 1' });
+  //   });
+  // });
 });
